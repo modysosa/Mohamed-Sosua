8c59a7791e526fb839e684cdacbc5178ed3feb2f
modysosa <modysosa@hotmail.com>
2024-11-16 00:23:06 +0300

    fullScreen

--- stage/js/main.js
+++ stage/js/main.js
@@ -15,4 +15,41 @@ $(function(){
           $(this).next('.dropdown-menu').toggleClass('show');
       });
   });
+  // open / close fulscreen
+  $(".toggle-fullscreen").on("click", function(){
+    $(this).toggleClass("full-screen");
+    if($(this).hasClass("full-screen")){ // page now is full screen
+        openFullscreen();
+    } else{  // page is not full screen
+        closeFullscreen();
+    }
+  });
 });
+/* Get the element you want displayed in fullscreen */ 
+var elem = document.documentElement;
+
+/* Function to open fullscreen mode */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Function to close fullscreen mode */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) { /* Firefox */
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+}
