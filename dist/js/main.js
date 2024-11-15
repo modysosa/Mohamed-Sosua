a384f8627159f3e873dd76bf794ecc06fa33f7a1
modysosa <modysosa@hotmail.com>
2024-11-15 03:51:29 +0300

    Update GitHub Pages source to main brance1

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,2 +1,2 @@
-$(function(){$(".toggle-sidebar").on("click",function(){$(".content-area, .sidebar").toggleClass("no-sidebar")})});
+$(function(){$(".toggle-sidebar").on("click",function(){$(".content-area, .sidebar").toggleClass("no-sidebar")}),$(".toggle-submenu").on("click",function(){$(this).find(".fa-angle-right").toggleClass("down"),$(this).next(".child-links").slideToggle()})});
 //# sourceMappingURL=main.js.map
